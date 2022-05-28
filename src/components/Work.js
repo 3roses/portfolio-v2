3771369bde3796fa97d4189c38a623caa63541eb
Craig Leslie <cleslie25@gmail.com>
2022-05-28 15:08:05 -0500

    styling for Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useEffect } from 'react';
 import AOS from 'aos';
+import 'aos/dist/aos.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import nba from '../photos/nba-proj-screenshot.png';
+import { faFacebook, faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
+import nba from '../photos/nba/nba-screenshot4.png';
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
     
 
@@ -23,20 +25,20 @@ const Work = () => {
   return (
     <>
         <div className='projects'>
-            <div className='singleProject'>
-                <div className='projectPictute'>
-                    <img src={nba} alt='abstract painting'/>
+            <div className='singleProject'> 
+                <div  data-aos='fade-right' data-aos-duration="1000" data-aos-delay='50' className='projectPicture'>
+                    <img src={nba}  alt='abstract painting'/>
                 </div>
-                <div className='projectInfo'>
-                    <h1>Jason Smith</h1>
-                    <h2>Contained Splash <span>c.('21)</span></h2>
-                    <p className='artistAboutUnderline'>About the piece:</p>
-                    <p className='artistAboutInfo'>Inspired by a psychedelic walk on the beach, this piece portrays the coexistence of fluidity and geometric structure. Many people assume the two don't mix, but Jason Smith would disagree.</p>
-                    <p className='artistGalleryPrice'>The current bid is: SOLD</p>
+                <div data-aos='fade-left' data-aos-duration="1000" data-aos-delay='50' className='projectInfo'>
+                    <h1>"NBA Fandom"</h1>
+                    <p className='projectAboutInfo'>This app is a one-stop-shop for NBA stats and schedules. The schedule for current and next 5 days is displayed in the cards on the bottom of the page. Type in a player's name in the search bar and a table is created with their stats. This is a useful tool for comparing players. Choose a team from the dropdown menu to see a complete roster for that team and statistics for each player. </p>
+                    <p className='projectDeployInfo'>Check out the deployed app!</p>
+                    <div className='deployBtn'>
+                    <a href='https://eugene125.github.io/project-1/index.html' target='_blank' rel="noreferrer"><Button>NBA Fandom</Button></a>
+                    </div>
+                    <p className='projectDeployInfo'>And take a look at the code on GitHub!</p>
                     <div className='socials'>
-                        <a href='https://twitter.com/' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faTwitter} className='spaceIcon'></FontAwesomeIcon></a>
-                        <a href='https://www.instagram.com/' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className='spaceIcon'></FontAwesomeIcon></a>
-                        <a href='https://www.facebook.com/' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faFacebook} className='spaceIcon'></FontAwesomeIcon></a>
+                        <a href='https://twitter.com/' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faGithub} className='spaceIcon'></FontAwesomeIcon></a>
                     </div>
                 </div>
             </div>
