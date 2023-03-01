f371284ed194aa7475cdeefc8d50071b371288b9
Craig Leslie <cleslie25@gmail.com>
2023-03-01 12:25:43 -0600

    changed alt to title for iframe

--- src/components/Work.js
+++ src/components/Work.js
@@ -34,7 +34,7 @@ const Work = () => {
         <div className='singleProject'> 
                 <div  data-aos='fade-right' data-aos-duration="1000" data-aos-delay='50' className='projectPicture'>
                     <div className='vidContainer'>
-                        <iframe class="responsive-iframe" src="https://drive.google.com/file/d/16pZTZVOLXYWI1OYUYikEYvr0MnN7zNlP/preview" width="680" height="510" alt="32edu walkthrough video"></iframe>
+                        <iframe class="responsive-iframe" src="https://drive.google.com/file/d/16pZTZVOLXYWI1OYUYikEYvr0MnN7zNlP/preview" width="680" height="510" title="32edu walkthrough video"></iframe>
                     </div>
                 </div>
                 <div data-aos='fade-left' data-aos-duration="1000" data-aos-delay='50' className='projectInfo'>
